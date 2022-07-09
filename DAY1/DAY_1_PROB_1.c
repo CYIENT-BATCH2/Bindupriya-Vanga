f3b7862c650ba1c811b8825298ad9b2e20653961
Bindupriya9827 <108974219+Bindupriya9827@users.noreply.github.com>
2022-07-09 15:23:14 +0530

    Create DAY_1_PROB_1.c

---
+++ DAY1/DAY_1_PROB_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    printf("enter a number:");
+    i=i+1;
+    scanf("%d",&i);
+    
+    if((i%11==0)||(i%11==1))
+    {
+    printf("special:");
+    }
+    else
+    printf("not special:");
+ }
