011643aca8624578f39f1cc01e6e2ea2990fd125
Bindupriya9827 <108974219+Bindupriya9827@users.noreply.github.com>
2022-07-12 21:13:49 +0530

    Create DAY_1_PROB_2.c

---
+++ DAY1/DAY_1_PROB_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+      char z;
+    printf("enter a char");
+      scanf("%c",&z);
+    if((z=='a')||(z=='e')||(z=='i')||(z=='o')||(z=='u')||(z=='A')||(z=='E')||(z=='I')||(z=='O')||(z=='U'))
+    {
+    printf("vowel");
+    }
+    else
+    printf("consonent");
+  return 0;
+ }
