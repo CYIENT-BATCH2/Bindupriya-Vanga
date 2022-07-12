4340703a66eb853f5687be8230e03a5cd504b726
Bindupriya9827 <108974219+Bindupriya9827@users.noreply.github.com>
2022-07-12 21:19:51 +0530

    Create DAY_1_PROB_3.c

---
+++ DAY1/DAY_1_PROB_3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+      float r,a;
+     float pi=3.14;
+        printf("enter the radius:");
+        scanf("%f",&r);
+        a=pi*r*r;
+        printf("%f",a);
+}
