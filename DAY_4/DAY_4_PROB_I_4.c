9bde73afbd6038ffbb94dd025de77b89677718c5
Bindupriya9827 <108974219+Bindupriya9827@users.noreply.github.com>
2022-07-14 23:00:03 +0530

    Create DAY_4_PROB_I_4.c

---
+++ DAY_4/DAY_4_PROB_I_4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int checkPowerOf2(unsigned int num)
+{
+    if ((num & (num - 1)) != 0)
+        return 0;
+    return 1;
+}
+int main()
+{
+    unsigned int num = 0;
+    scanf("%d", &num);
+    if (checkPowerOf2(num))
+        printf("Given number is power of 2.\n");
+    else
+        printf("Given number is not power of 2.\n");
+    return 0;
+}
