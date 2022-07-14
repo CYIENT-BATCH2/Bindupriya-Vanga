4cc6ccd1cab61a099af17ab690e1b29f9a7d6546
Bindupriya9827 <108974219+Bindupriya9827@users.noreply.github.com>
2022-07-14 14:45:42 +0530

    Delete DAY_3_PROB_8.c

--- DAY3/DAY_3_PROB_8.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	char ch;
-  printf("enter variable:");
-	scanf("%c",&ch);
-	((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))?printf("is alphabet"):printf("is not an alphabet");
-}
